5884de084cb35308cdc7a4ad28511da4ecf5910e
yudonghun <donghun15.yu@samsung.com>
2021-06-04 16:15:43 +0900

    ♻️ 레포지토리 클래스 프로퍼티를 생성자로 이동시켜 주입을 하도록 변경

--- src/main/kotlin/com/github/donghune/companyz/money/MoneyFeature.kt
+++ src/main/kotlin/com/github/donghune/companyz/money/MoneyFeature.kt
@@ -3,24 +3,32 @@ package com.github.donghune.companyz.money
 import com.github.donghune.companyz.util.struct.Feature
 import com.github.donghune.companyz.money.command.MoneyCommand
 import com.github.donghune.companyz.money.listener.MoneyListener
+import com.github.donghune.companyz.money.model.PlayerMoney
 import com.github.donghune.companyz.money.model.PlayerMoneyRepository
+import com.github.donghune.companyz.plugin
 import com.github.donghune.companyz.util.struct.Command
 import org.bukkit.event.Listener
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import java.io.File
 
 class MoneyFeature : Feature() {
 
     override val commands: List<Command> = listOf(
-            MoneyCommand()
+        MoneyCommand()
     )
 
     override val listeners: List<Listener> = listOf(
-            MoneyListener()
+        MoneyListener()
     )
 
     override val modules: Module = module {
-        single { PlayerMoneyRepository() }
+        single {
+            PlayerMoneyRepository(
+                PlayerMoney::class.java,
+                File("${plugin.dataFolder.absolutePath}/money/players")
+            )
+        }
     }
 
 }
