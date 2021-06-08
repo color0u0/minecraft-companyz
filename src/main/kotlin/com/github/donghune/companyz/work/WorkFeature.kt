3bb758048944b26f0441b9bb03302b787a4e9221
namu <reona7140@naver.com>
2021-06-08 23:48:33 +0900

    🚑️ 클래스를 지정하지 않아 직렬화가 되지 않던 버그 수정

--- src/main/kotlin/com/github/donghune/companyz/work/WorkFeature.kt
+++ src/main/kotlin/com/github/donghune/companyz/work/WorkFeature.kt
@@ -1,20 +1,16 @@
 package com.github.donghune.companyz.work
 
-import com.github.donghune.companyz.work.model.Work
-import com.github.donghune.companyz.work.model.WorkRepository
-import com.github.donghune.companyz.work.model.WorkReward
 import com.github.donghune.companyz.plugin
 import com.github.donghune.companyz.util.struct.Command
 import com.github.donghune.companyz.util.struct.Feature
 import com.github.donghune.companyz.work.command.WorkCommand
 import com.github.donghune.companyz.work.listener.WorkListener
-import com.github.donghune.companyz.work.model.PartTimeWorkRepository
+import com.github.donghune.companyz.work.model.*
 import org.bukkit.configuration.serialization.ConfigurationSerializable
 import org.bukkit.event.Listener
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import java.io.File
-import kotlin.math.sin
 
 class WorkFeature : Feature() {
     override val commands: List<Command> = listOf(
@@ -40,6 +36,7 @@ class WorkFeature : Feature() {
 
     override val serializableClazzs: List<Class<out ConfigurationSerializable>> = listOf(
         Work::class.java,
-        WorkReward::class.java
+        WorkMission::class.java,
+        WorkReward::class.java,
     )
 }
